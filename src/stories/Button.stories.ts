11d0b0b0af454b82ae8d81129a0de3c9da0bb35e
LucasPapini <papine12345@hotmail.com>
2023-05-26 16:01:14 -0300

    configurações do storybook e avanço na criação de componentes documentados

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,56 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import Button, { ButtonProps } from '../components/Button/Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-//const Template: Story<ButtonProps> = (args) => <Button { ...args } />
-
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Primary: Story = {
-  // More on args: https://storybook.js.org/docs/react/writing-stories/args
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    variant: 'primary',
+    label: 'resetar senha',
+    disabled: false,
   },
 };
 
 export const Danger: Story = {
   args: {
-    backgroundColor: 'red',
-    label: 'Danger',
+    variant: 'danger',
+    label: 'resetar senha',
+    disabled: false,
+  },
+};
+
+export const Text: Story = {
+  args: {
+    variant: 'text',
+    label: 'resetar senha',
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    variant: 'primary',
+    label: 'resetar senha',
+    disabled: false,
   },
 };
